eefba630dd7a418037ca409cec80db3b3e1a5c17
xiaoshangmin <Xsmxsm0220@gmail.com>
2022-07-21 19:14:55 +0800

    测试删除

--- app/Controller/TestController.php
+++ app/Controller/TestController.php
@@ -3,20 +3,42 @@
 namespace App\Controller;
 
 use Hyperf\Elasticsearch\ClientBuilderFactory;
+use Hyperf\Filesystem\FilesystemFactory;
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\GetMapping;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\StorageAttributes;
 
 #[Controller(prefix: "api/test")]
 class TestController extends AbstractController
 {
 
+    #[GetMapping(path: "test")]
+    public function test(FilesystemFactory $factory)
+    {
+        $local = $factory->get('local');
+        $list = $local->listContents('/office', true)->filter(fn(StorageAttributes $attributes) => $attributes->isFile())->toArray();
+        try {
+            foreach ($list as $item) {
+                if (time() - $item->lastModified() > 4 * 3600) {
+                    $dir = dirname($item->path());
+                    $local->deleteDirectory($dir);
+                }
+            }
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+
+    }
+
     #[GetMapping(path: "es")]
-    public function es(){
+    public function es()
+    {
         $builder = $this->container->get(ClientBuilderFactory::class)->create();
         $client = $builder->setHosts(['http://elasticsearch:9200'])->build();
         $params = [
             'index' => 'info',
-            'body'  => [
+            'body' => [
                 'query' => [
                     'match' => [
                         'testField' => 'abc'
@@ -27,8 +49,8 @@ class TestController extends AbstractController
         $response = $client->search($params);
         $params = [
             'index' => 'my_index',
-            'id'    => 'my_id',
-            'body'  => ['testField' => 'abc']
+            'id' => 'my_id',
+            'body' => ['testField' => 'abc']
         ];
         $response = $client->index($params);
 //        $params = [
