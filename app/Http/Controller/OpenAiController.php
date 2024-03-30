4611568c26b034385b7a5648960ddb0bda4e96e7
shangminxiao <shangminxiao@gmail.com>
2024-03-31 01:40:47 +0800

    test

--- app/Http/Controller/OpenAiController.php
+++ app/Http/Controller/OpenAiController.php
@@ -5,45 +5,18 @@ declare(strict_types=1);
 namespace App\Http\Controller;
 
 use Hyperf\HttpServer\Annotation\Controller;
-use Hyperf\HttpServer\Annotation\GetMapping;
-use Orhanerday\OpenAi\OpenAi;
+use Hyperf\HttpServer\Annotation\GetMapping; 
+use thiagoalessio\TesseractOCR\TesseractOCR;
 
-#[Controller(prefix: "api/openai")]
+#[Controller(prefix: "api/ocr")]
 class OpenAiController extends BaseController
 {
 
     #[GetMapping(path: "chat")]
-    public function chat(){
-        $open_ai_key = "";//getenv('OPENAI_API_KEY');
-        $open_ai = new OpenAi($open_ai_key);
-
-        $complete = $open_ai->chat([
-            'model' => 'gpt-3.5-turbo',
-            'messages' => [
-                [
-                    "role" => "system",
-                    "content" => "You are a helpful assistant."
-                ],
-                [
-                    "role" => "user",
-                    "content" => "Who won the world series in 2020?"
-                ],
-                [
-                    "role" => "assistant",
-                    "content" => "The Los Angeles Dodgers won the World Series in 2020."
-                ],
-                [
-                    "role" => "user",
-                    "content" => "Where was it played?"
-                ],
-            ],
-            'temperature' => 1.0,
-            'max_tokens' => 4000,
-            'frequency_penalty' => 0,
-            'presence_penalty' => 0,
-        ]);
-
-        return $complete;
+    public function chat(){ 
+        $storage = BASE_PATH . '/storage/pdf/123.jpg';
+        return (new TesseractOCR($storage))
+            ->run();
     }
 
 }
