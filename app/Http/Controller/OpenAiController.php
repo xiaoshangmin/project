71dfbc47fe04136e084d9e0424f98f6a7e2e3c82
shangminxiao <shangminxiao@gmail.com>
2024-04-02 23:39:24 +0800

    u

--- app/Http/Controller/OpenAiController.php
+++ app/Http/Controller/OpenAiController.php
@@ -5,18 +5,34 @@ declare(strict_types=1);
 namespace App\Http\Controller;
 
 use Hyperf\HttpServer\Annotation\Controller;
-use Hyperf\HttpServer\Annotation\GetMapping; 
+use Hyperf\HttpServer\Annotation\GetMapping;
 use thiagoalessio\TesseractOCR\TesseractOCR;
+use Gotenberg\Gotenberg;
+use Gotenberg\Stream;
+
 
 #[Controller(prefix: "api/ocr")]
 class OpenAiController extends BaseController
 {
 
     #[GetMapping(path: "chat")]
-    public function chat(){ 
-        $storage = BASE_PATH . '/storage/pdf/text.png';
+    public function chat()
+    {
+        $storage = BASE_PATH . '/storage/pdf/123.jpg';
         return (new TesseractOCR($storage))
             ->run();
+    }
+
+    #[GetMapping(path: "pdf")]
+    public function pdf()
+    {
+//        $filename = Gotenberg::save(
+//            Gotenberg::chromium("http://gotenberg:3000")->pdf()->outputFilename("mypdf")->url('https://baidu.com'),
+//            BASE_PATH . '/storage/pdf'
+//        );
+        $request = Gotenberg::libreOffice("http://gotenberg:3000")
+            ->convert(Stream::path(BASE_PATH . '/storage/pdf/2023.docx'));
+        Gotenberg::save($request, BASE_PATH . '/storage');
     }
 
 }
