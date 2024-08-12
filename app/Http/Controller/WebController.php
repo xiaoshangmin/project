e0e9d2b70652bb51e6f43098b3e6332472559a27
xiaoshangmin <Xsmxsm0220@gmail.com>
2024-08-12 14:52:50 +0800

    u

---
+++ app/Http/Controller/WebController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controller;
+
+use App\Constants\ErrorCode;
+use GuzzleHttp\Exception\GuzzleException; 
+use GuzzleHttp\Exception\RequestException;
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Guzzle\ClientFactory;
+use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\GetMapping;
+use Hyperf\HttpServer\Annotation\PostMapping;
+use Psr\Http\Message\ResponseInterface;
+
+#[Controller(prefix: "api/web")]
+class WebController extends BaseController
+{
+
+    #[Inject]
+    public ClientFactory $clientFactory;
+
+
+    #[GetMapping(path: "/og")]
+    public function queryOgInfo(): array
+    {
+        $url = $this->request->input("url", "");
+        try {
+            $client = $this->clientFactory->create([
+                'timeout' => 10,
+                'verify' => false,
+                'allow_redirects' => true
+            ]);
+            $uri = 'https://metascraper.pages.dev?url=' .  $url;
+            $response = $client->request(
+                'GET',
+                $uri
+            )->getBody()->getContents();
+            $response = json_decode($response, true); 
+            return $response;
+        } catch (RequestException $e) {
+            $this->logger->info("queryOgInfo curl RequestException=" . $e->getMessage());
+            return null;
+        } catch (GuzzleException $e) {
+            $this->logger->info("queryOgInfo curl GuzzleException=" . $e->getMessage());
+            return null;
+        }
+    }
+}
